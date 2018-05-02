51033e82af5df14ad87409e10f8df82a6baecd19
matan <matan@insoundz.com>
2018-05-02 21:04:55 +0300

    changes due to the requirements

--- app.py
+++ app.py
@@ -5,15 +5,16 @@ import logging
 import json
 from pathlib import Path
 from requests_futures.sessions import FuturesSession
+from PIL import Image
 
 session = FuturesSession()
 
 app = Flask(__name__)
 
 face_api_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect'
+find_similar_url = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/findsimilars'
 headers = {
-    'Ocp-Apim-Subscription-Key': os.environ['AZURE_FACE_API_KEY'],
-    'Content-Type': 'application/octet-stream'
+    'Ocp-Apim-Subscription-Key': os.environ['AZURE_FACE_API_KEY']
 }
 
 
@@ -42,25 +43,31 @@ def remove_no_detections(response):
     return valid_detections
 
 
-def get_best_image(results):
-    meta_data = {}
-    max_size = 0
-    largest_detection = None
-    largest_detection_filename = None
-    for file_name, result in results.items():
-        for detection in result:
-            temp_box_size = bounding_box_size(detection)
-            if temp_box_size > max_size:
-                largest_detection_filename = file_name
-                largest_detection = detection
-                max_size = temp_box_size
-    meta_data[largest_detection_filename] = largest_detection
-    return meta_data
+def get_best_image(most_common_files):
+    ratio_detection = 0
+    best_detection = {}
+    best_image_filename = None
+    for file_name, detections in most_common_files.items():
+        if len(detections) == 2:
+            if bounding_box_size(detections[0]) > bounding_box_size(detections[1]):
+                return {file_name: detections[0]}
+            else:
+                return {file_name: detections[1]}
+        else:
+            image = Image.open('./faces/' + file_name)
+            image_width, image_height = image.size
+            ratio = bounding_box_size(detections[0]) / (image_width * image_height)
+            if ratio > ratio_detection:
+                ratio_detection = ratio
+                best_detection = detections[0]
+                best_image_filename = file_name
+    return {best_image_filename: best_detection}
 
 
 def get_face_detection(binary_file_list):
     results = {}
     responses = {}
+    headers['Content-Type'] = 'application/octet-stream'
     for file_name, data in binary_file_list.items():
         responses[file_name] = session.post(face_api_url, params={}, headers=headers, data=data)
     for file_name, response in responses.items():
@@ -69,7 +76,7 @@ def get_face_detection(binary_file_list):
             r.raise_for_status()
             results[file_name] = json.loads(r.text)
         except requests.exceptions.RequestException as e:
-            logging.error("Error while getting Face API detection for {0}: {1}".format(file_name, e))
+            logging.error("Error while getting Face API detection for {0}: {1}".format(file_name, e.strerror))
     return results
 
 
@@ -80,8 +87,68 @@ def read_files(files):
             data = open('./faces/' + file, 'rb').read()
             binary_file_list[file] = data
         except OSError as e:
-            logging.error("Error while reading file {0}: {1}".format(file, e))
+            logging.error("Error while reading file {0}: {1}".format(file, e.strerror))
     return binary_file_list
+
+
+def get_face_ids(detections_response):
+    face_ids = {}
+    for file_name, detections in detections_response.items():
+        face_ids[file_name] = [x['faceId'] for x in detections]
+    return face_ids
+
+
+def get_face_ids_array(face_ids):
+    face_ids_array = []
+    for file_name, ids in face_ids.items():
+        face_ids_array.extend(ids)
+    return face_ids_array
+
+
+def get_similarities(face_ids_array):
+    results = {}
+    for i in range(0, len(face_ids_array)):
+        face_id = face_ids_array[i]
+        face_ids = [x for j, x in enumerate(face_ids_array) if i != j]
+        try:
+            headers['Content-Type'] = 'application/json'
+            r = requests.post(find_similar_url, params={}, headers=headers, json={
+                "faceId": face_id,
+                "faceIds": face_ids,
+                "mode": "matchFace",
+                "maxNumOfCandidatesReturned": 1
+            })
+            r.raise_for_status()
+            results[face_id] = json.loads(r.text)
+        except requests.exceptions.RequestException as e:
+            logging.error("Error while getting Face API detection for {0}".format(e.strerror))
+    return results
+
+
+def get_best_similarity(similarities):
+    face_id_match = None
+    confidence = 0
+    for face_id, similarity in similarities.items():
+        if similarity[0]['confidence'] > confidence:
+            confidence = similarity[0]['confidence']
+            face_id_match = face_id
+    if face_id_match is None:
+        return {}
+    else:
+        return {face_id_match: similarities[face_id_match]}
+
+
+def get_most_common_files(detections_response, best_similarity):
+    most_common_files = {}
+    face_id_one, face_id_two = list(best_similarity.keys())[0], list(best_similarity.items())[0][1][0]['faceId']
+    for file_name, detections in detections_response.items():
+        for detection in detections:
+            if detection['faceId'] == face_id_one or detection['faceId'] == face_id_two:
+                if file_name not in most_common_files:
+                    most_common_files[file_name] = [detection]
+                else:
+                    most_common_files[file_name].append(detection)
+    return most_common_files
 
 
 @app.route("/healthcheck")
@@ -108,13 +175,31 @@ def get():
 
     response = get_face_detection(binary_file_list)
     if response == {}:
+        logging.error("ERROR: all call to face detection api failed")
         return 'Internal Server Error', 500
 
     detections_response = remove_no_detections(response)
     if detections_response == {}:
         return "Face API found 0 detections", 200
 
-    meta_data = get_best_image(detections_response)
+    face_ids = get_face_ids(detections_response)
+
+    face_ids_array = get_face_ids_array(face_ids)
+    if len(face_ids_array) == 1:
+        return 'There is only one face in photos, file: {0}'.format(list(face_ids.keys())[0]), 200
+
+    similarities = get_similarities(face_ids_array)
+    if similarities == {}:
+        logging.error("ERROR: all call to face similarity api failed")
+        return 'Internal Server Error', 500
+
+    best_similarity = get_best_similarity(similarities)
+    if best_similarity == {}:
+        return 'Face API found 0 similarities', 200
+
+    most_common_files = get_most_common_files(detections_response, best_similarity)
+
+    meta_data = get_best_image(most_common_files)
 
     return jsonify(meta_data)
 
